6ad59ac3db002c84378dda8daaa476a342901dd5
VTAbhishek <visaltheekshana555@gmail.com>
2025-01-28 00:05:35 +0530

    Delete Xgenic/Xgenic directory

--- Xgenic/Xgenic/chart.js
+++
@@ -1,7 +0,0 @@
-formatter: (value, ctx) => {
-    let sum = 0;
-    const dataArr = ctx.chart.data.datasets[0].data;
-    dataArr.forEach(data => sum += data);
-    const percentage = (value * 100 / sum).toFixed(2) + '%';
-    return percentage;
-}
